6c0df0be382ff15c0b40633b41158bf7c67ccec2
Mikita Manko <gmail@MikitaManko.com>
2014-03-28 00:24:56 -0700

    added basic os detection

--- src/bsInf.js
+++ src/bsInf.js
@@ -11,10 +11,10 @@
  ~function(context, libraryName, document, navigator){
     var bsInf = context[libraryName] = {};
 
-    bsInf.browser = getBrowser();
-    bsInf.os = getOs();
+    bsInf.browser = getBrowser(navigator.userAgent, window);
+    bsInf.os = getOs(navigator.userAgent);
 
-    function getBrowser(){
+    function getBrowser(ua, window){
       var result = {};
       var browsers = [
         { name: 'chrome', test: /Chrome\/([\d.]+)/ }
@@ -26,27 +26,49 @@
         ,{ name: 'ie', test: /Trident\/[\d](?=[^\?]+).*rv:([0-9.].)/}
       ];
 
-      for(var i = 0, len = browsers.length; i < len; i++) {
-        var match = navigator.userAgent.match(browsers[i].test);
-        if(!!match) {
+      for(var i = 0, match, len = browsers.length; i < len; i++) {
+        if(!!(match = ua.match(browsers[i].test))) {
           result.name = browsers[i].name;
           result.version = match[1];
           return result;
         }
       }
-
       if(!(window.ActiveXObject) && "ActiveXObject" in window) {
         result.name = 'ie';
         result.version = '11';
       }
-
       return result;
     }
+    // Make available to unit tests.
+    bsInf.__getBrowser = getBrowser;
 
-    function getOs(){
+    function getOs(ua){
       var result = {};
-      // TODO: implement it.
+      var oss = [
+        {name:'osx', test: /\(Macintosh\; Intel /}
+        ,{name: 'android', test: /(Android);?[\s\/]+([\d.]+)?/}
+        ,{name: 'webos', test: /(webOS|hpwOS)[\s\/]([\d.]+)/}
+        ,{name: 'kindle', test: /Kindle\/([\d.]+)/}
+        ,{name: 'blackberry', test: /(BlackBerry).*Version\/([\d.]+)/}
+        ,{name: 'playbook', test: /PlayBook/}
+        ,{name: 'rimtabletos', test: /(RIM\sTablet\sOS)\s([\d.]+)/}
+        ,{name: 'bb10', test: /(BB10).*Version\/([\d.]+)/}
+        ,{name: 'silk', test: /Silk\/([\d._]+)/}
+      ];
+
+      for(var i = 0, match, len = oss.length; i < len; i++) {
+        if(!!(match = ua.match(oss[i].test))) {
+          result.name = oss[i].name;
+          result.version = match[2] || match[1] || '-';
+          return result;
+        }
+      }
+
+      // TODO: implement it for windows.
       return result;
     }
+    // Make available to unit tests.
+    bsInf.__getOs = getOs;
+
 
  }(window, 'bsInf', document, navigator);
